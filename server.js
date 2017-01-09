0fcb237939272f89b4faaafc7c2c816777755743
Fejes Aniko <oranjje@gmail.com>
2017-01-09 18:43:02 +0100

    post endpoint

--- server.js
+++ server.js
@@ -8,4 +8,14 @@ app.get('/', function(req, res) {
   res.sendfile('./client/index.html');
 });
 
+app.post('/decode', function(req, res) {
+  var probavalami = {
+    "status": "ok",
+    "text": "lorem ipsum dolor sit"
+  };
+  res.send(probavalami)
+});
+
+
+
 app.listen(3000);
