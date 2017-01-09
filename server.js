a4dcbbe26a30a8a5f00f96086dd870b68bef40e8
Fejes Aniko <oranjje@gmail.com>
2017-01-09 18:56:26 +0100

    endpoints scaffolding: get, post

--- server.js
+++ server.js
@@ -1,19 +1,41 @@
 'use strict';
 
 var express = require('express');
+var bodyParser = require('body-parser');
+
+function decrypt (shift, text) {
+  return text;
+}
 
 var app = express();
+app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
   res.sendfile('./client/index.html');
 });
 
 app.post('/decode', function(req, res) {
-  var probavalami = {
+  var shift = req.body.shift;
+  var text = req.body.text;
+  var decrypted = decrypt(shift, text);
+
+  var response = {
     "status": "ok",
-    "text": "lorem ipsum dolor sit"
+    "text": decrypted
+  }
+
+  res.send(response);
+});
+
+app.get('/decode/all', function(req, res) {
+  var probavalami = {
+    "all": [
+      "lorem ipsum dolor sit",
+      "consectetur adipisicing elit",
+      "ut labore et dolore magna aliqua"
+    ]
   };
-  res.send(probavalami)
+  res.send(probavalami);
 });
 
 
